9a787c2d2a99cd2fc3603df6a02667e13eae494b
Anushkaa0504 <165053721+Anushkaa0504@users.noreply.github.com>
2025-04-26 17:50:27 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
    <footer>
-    <p>Created By <a href="https://ankitjha.vercel.app" target='_blank'>Ankit Jha</a></p>
+    <p>Created By <a href="https://ankitjha.vercel.app" target='_blank'>Anushka and Ansh</a></p>
    </footer>
   )
 }
